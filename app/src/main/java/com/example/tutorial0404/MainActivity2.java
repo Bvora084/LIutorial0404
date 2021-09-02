54cae060cee8f1519c20b2761bd359f4548662e3
Brijesh Vora <85928806+Bvora084@users.noreply.github.com>
2021-09-02 12:27:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tutorial0404/MainActivity2.java
@@ -0,0 +1,27 @@
+package com.example.tutorial0404;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView textwelcome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        textwelcome = findViewById(R.id.textwelcome);
+
+        Intent intent = getIntent();
+        String userdatavalue = intent.getStringExtra("userdata");
+        textwelcome.setText("welcome" + userdatavalue);
+
+        Toast.makeText(this, userdatavalue, Toast.LENGTH_SHORT).show();
+
+    }
+}
